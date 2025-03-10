46718d3d5da2f73b12c7d208af659ff705f036cc
izcheung <icheung24@my.bcit.ca>
2025-03-09 18:36:36 -0700

    add reservation page boilerplate

---
+++ thetastyfork/src/pages/Reservation/Reservation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Reservation.css";
+import Calendar from "../../components/Calendar/Calendar";
+
+const Reservation = () => {
+  return (
+    <div>
+      Reservation
+      <Calendar />
+    </div>
+  );
+};
+
+export default Reservation;
