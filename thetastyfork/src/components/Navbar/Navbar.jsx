f0e185eb947136e56ca6685c3f921de7ce6b3231
izcheung <icheung24@my.bcit.ca>
2025-03-10 06:39:11 -0700

    set default page to home

--- thetastyfork/src/components/Navbar/Navbar.jsx
+++ thetastyfork/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import assets from "../../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("menu");
+  const [menu, setMenu] = useState("home");
   const navigate = useNavigate();
 
   const handleClick = () => {
