7aa9be1f14d7673b3ec1fbb414ea0726a086d612
izcheung <icheung24@my.bcit.ca>
2025-03-09 21:03:33 -0700

    add asset exports

---
+++ thetastyfork/src/assets/assets.jsx
@@ -0,0 +1,23 @@
+import food from "./food.jpg";
+import food1 from "./food1.jpg";
+import food2 from "./food2.jpg";
+import food3 from "./food3.jpg";
+import food4 from "./food4.jpg";
+import food5 from "./food5.jpg";
+import food6 from "./food6.png";
+import food8 from "./food8.jpg";
+import logo from "./logo.png";
+
+const assets = {
+  food,
+  food1,
+  food2,
+  food3,
+  food4,
+  food5,
+  food6,
+  food8,
+  logo,
+};
+
+export default assets;
