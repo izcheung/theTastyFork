fbdd9f65de3e2d10c5427f6998d16be0fd87a184
izcheung <icheung24@my.bcit.ca>
2025-03-09 18:39:01 -0700

    add footer

---
+++ thetastyfork/src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import assets from "../../assets/assets";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} width="70px" height="70px" />
+          <p>Loreamfdklsajfl</p>
+        </div>
+
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+
+        <div className="footer-content-right">
+          <h2>Get in touch</h2>
+          <ul>
+            <li>123-456-7890</li>
+            <li>email@example.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr></hr>
+      <p className="footer-copyright">Copyright 2025</p>
+    </div>
+  );
+};
+
+export default Footer;
