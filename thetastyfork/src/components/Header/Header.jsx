7d8f9dd6c93291d9a64df4541b400849b5e94145
izcheung <icheung24@my.bcit.ca>
2025-03-09 18:39:26 -0700

    make header for website

---
+++ thetastyfork/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>THE TASTY FORK</h2>
+        <p>Where Elegance Meets Flavor</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
