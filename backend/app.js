75ada10a3318f75a7d9fa723ef7f4c1a0651c5b7
Hsin Pang <hpang19@my.bcit.ca>
2025-03-10 19:01:57 -0700

    added backend for submitting a review

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+const reviewRoutes = require('./review'); 
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join('thetastyfork', 'src')));
+
+app.use('/submitReview', reviewRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ backend/review.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+const multer = require('multer');
+const upload = multer();
+const config = require('./config');
+
+const router = express.Router();
+
+AWS.config.update(config.awsConfig);
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+const REVIEW_TABLE = 'Reviews';
+const S3_BUCKET_NAME = 'comp3962-milestone-review-bucket';
+
+router.post('/', upload.single('photo'), async (req, res) => {
+  try {
+    const { reviewTitle, foodRating, serviceRating, overallExperience, name, email, reviewContent } = req.body;
+    const photo = req.file;
+
+    let photoUrl = '';
+    
+    // if (photo) {
+    //   const uploadParams = {
+    //     Bucket: S3_BUCKET_NAME,
+    //     Key: `reviews/${uuid.v4()}_${photo.originalname}`,
+    //     Body: photo.buffer,
+    //     ContentType: photo.mimetype,
+    //   };
+
+    //   const uploadResult = await s3.upload(uploadParams).promise();
+    //   photoUrl = uploadResult.Location;
+    // }
+
+    const reviewItem = {
+      TableName: REVIEW_TABLE,
+      Item: {
+        reviewId: uuid.v4(),
+        reviewTitle,
+        reviewContent,
+        foodRating,
+        serviceRating,
+        overallExperience,
+        photoUrl,
+        name,
+        email,
+        createdAt: new Date().toISOString(),
+      },
+    };
+
+    await dynamoDB.put(reviewItem).promise();
+
+    res.status(200).json({ message: 'Review submitted successfully!' });
+  } catch (error) {
+    console.error('Error submitting review:', error);
+    res.status(500).json({ message: 'Error submitting review' });
+  }
+});
+
+module.exports = router;
