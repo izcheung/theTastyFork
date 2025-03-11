d36d6337f6e86f9d08438fce816dbe88246d8ee7
Wanyi Zhu <wzhu36@my.bcit.ca>
2025-03-11 00:50:56 -0700

    Imported route handlers and defined API routes for review page and reservation page

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 
-const reviewRoutes = require('./review'); 
+const reviewRoutes = require('./review');
+const contactRoutes = require("./contact");
+const reservationRoutes = require("./reservation"); 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +16,9 @@ app.use(express.static(path.join('thetastyfork', 'src')));
 
 app.use('/submitReview', reviewRoutes);
 app.use('/fetchReviews', reviewRoutes);
+app.use("api/reservation", reservationRoutes);
+app.use("api/contact", contactRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
