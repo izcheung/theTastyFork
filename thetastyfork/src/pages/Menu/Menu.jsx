f4142b6438b3a2c3ddac5e9f6a1db2ac6f631666
izcheung <icheung24@my.bcit.ca>
2025-03-10 22:48:18 -0700

    populate menu page with food options

--- thetastyfork/src/pages/Menu/Menu.jsx
+++ thetastyfork/src/pages/Menu/Menu.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./Menu.css";
+import { menuitems } from "../../assets/assets";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <Container className="menu-container">
+      <h1 id="menu-title">Menu</h1>
+      <div className="menu-gallery">
+        {Object.keys(menuitems).map((key) => (
+          <Card sx={{ maxWidth: 345, borderRadius: "15px" }} key={key}>
+            <CardMedia
+              component="img"
+              height="194"
+              image={menuitems[key].image}
+            />
+            <CardContent>
+              <Typography variant="body2">
+                {menuitems[key].description}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Menu;
