aa0bee3b0be2d0e9109d45076dd18d07e556b99b
Wanyi Zhu <wzhu36@my.bcit.ca>
2025-03-28 22:38:09 -0700

    Update the actual file name in the import

--- thetastyfork/src/App.jsx
+++ thetastyfork/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar/navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
