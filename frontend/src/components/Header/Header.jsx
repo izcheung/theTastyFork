8e375e92ffe336a3a6232200629a06a146cc8ee9
izcheung <icheung24@my.bcit.ca>
2025-10-15 17:45:05 -0700

    fix: update pointer to background image

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./Header.css";
+import { assets } from "../../assets/assets";
 
 const Header = () => {
+  console.log("Image path:", assets.food9);
   return (
-    <div className="header">
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${assets.food9})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
       <div className="header-contents">
         <h2>THE TASTY FORK</h2>
         <p>Where Elegance Meets Flavor</p>
